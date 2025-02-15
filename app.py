a786423cfa182a6bbb472b0a71fdc5030023927e
vatsastitva <21f3003255@ds.study.iitm.ac.in>
2025-02-15 17:47:28 +0530

    made something

--- app.py
+++ app.py
@@ -1,2 +1,23 @@
 #Such a pain in the ass this project
 #again try
+
+
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+app=FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["GET", "POST"],
+    allow_headers=["*"],
+)
+
+@app.get("/")
+def home():
+    return {"message":"Hello World"}
+
+if __name__ == '__main__':
+    import uvicorn
+    uvicorn.run(app, host="0.0.0.0", port=8000)
